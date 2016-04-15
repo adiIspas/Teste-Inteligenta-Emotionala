5ad4d47f4fa6885cd0a4ed67b52c8e23ede7bab6
adiIspas <adi.ispas95@gmail.com>
2016-04-15 09:18:21 +0300

    Finish Test Progress Activity

--- TesteInteligentaEmotionala/app/src/main/java/com/example/noi25/testeinteligentaemotionala/TestProgressActivity.java
+++ TesteInteligentaEmotionala/app/src/main/java/com/example/noi25/testeinteligentaemotionala/TestProgressActivity.java
@@ -9,50 +9,63 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.TextView;
+
 public class TestProgressActivity extends Activity {
-    List<Question> quesList;
-    int score=0;
-    int qid=0;
-    Question currentQ;
-    TextView txtQuestion;
-    RadioButton rda, rdb;
-    Button butNext;
+    List<Question> questionsList;
+    int score = 0;
+    int qid = 0;
+    Question currentQuestion;
+    TextView textQuestion;
+    RadioButton buttonYes, buttonNo;
+    Button buttonNext;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_progress_model_1);
-        DbHelper db=new DbHelper(this);
-        quesList=db.getAllQuestions();
-        currentQ=quesList.get(qid);
-        txtQuestion=(TextView)findViewById(R.id.textQuestion);
-        rda=(RadioButton)findViewById(R.id.buttonYes);
-        rdb=(RadioButton)findViewById(R.id.buttonNo);
-        butNext=(Button)findViewById(R.id.buttonNextQuestion);
+
+        DbHelper db = new DbHelper(this);
+        questionsList = db.getAllQuestions();
+        currentQuestion = questionsList.get(qid);
+
+        textQuestion = (TextView)findViewById(R.id.textQuestion);
+        buttonYes = (RadioButton)findViewById(R.id.buttonYes);
+        buttonNo = (RadioButton)findViewById(R.id.buttonNo);
+        buttonNext = (Button)findViewById(R.id.buttonNextQuestion);
+
         setQuestionView();
-        butNext.setOnClickListener(new View.OnClickListener() {
+
+        buttonNext.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 boolean isChecked = false;
-                if (rda.isChecked()) {
+
+                if (buttonYes.isChecked()) {
                     isChecked = true;
                 }
 
                 if (isChecked) {
-                    Log.d("raspuns", "r: " + Integer.parseInt(currentQ.getANSWER1()));
-                    score += Integer.parseInt(currentQ.getANSWER1());
-                    rda.setChecked(false);
-                } else {
-                    Log.d("raspuns", "r: " + Integer.parseInt(currentQ.getANSWER2()));
-                    score += Integer.parseInt(currentQ.getANSWER2());
-                    rdb.setChecked(false);
+                    Log.d("raspuns", "r: " + Integer.parseInt(currentQuestion.getANSWER1()));
+                    score += Integer.parseInt(currentQuestion.getANSWER1());
+
+                    buttonYes.setChecked(false);
+                    buttonYes.setSelected(false);
+                    buttonYes.setClickable(true);
+                }
+                else {
+                    Log.d("raspuns", "r: " + Integer.parseInt(currentQuestion.getANSWER2()));
+                    score += Integer.parseInt(currentQuestion.getANSWER2());
+
+                    buttonNo.setChecked(false);
+                    buttonNo.setSelected(false);
+                    buttonNo.setClickable(true);
                 }
 
                 if (qid < 3){
-                    currentQ=quesList.get(qid);
+                    currentQuestion=questionsList.get(qid);
                     setQuestionView();
                 }
-                //enuntIntrebare.setText(intrebari[intrebareCurenta]);
-                else{
+                else {
                     Intent intent = new Intent(getApplicationContext(),FinishTestActivity.class);
                     intent.putExtra("Scor",score);
                     startActivity(intent);
@@ -64,9 +77,9 @@ public class TestProgressActivity extends Activity {
 
     private void setQuestionView()
     {
-        txtQuestion.setText(currentQ.getQUESTION());
-        rda.setText(currentQ.getOPTA());
-        rdb.setText(currentQ.getOPTB());
+        textQuestion.setText(currentQuestion.getQUESTION());
+        buttonYes.setText(currentQuestion.getOPTA());
+        buttonNo.setText(currentQuestion.getOPTB());
         qid++;
     }
 }
